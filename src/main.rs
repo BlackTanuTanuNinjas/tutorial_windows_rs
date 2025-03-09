59d27090c42d37e52046aefbff8465fc2ccb6f79
BlackTanuTanuNinjas <development.tanuki2024@gmail.com>
2025-03-09 16:32:44 +0900

    メッセージボックスを表示

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// windowsにおいて、コンソールを開かずに実行
+#![cfg_attr(windows, windows_subsystem = "windows")]
+
+use windows::{core::*, Win32::UI::Shell::*, Win32::UI::WindowsAndMessaging::*};
+
+fn main() {
+    unsafe {
+        MessageBoxW(None, w!("末尾WはWin32APIのunicode対応!"), w!("タイトル"), MB_OK);
+        
+        ShellMessageBoxW(None, None, 
+            w!("ShellMessageBoxではアイコンと効果音が設定可能!\n"
+               "4つ目の引数で、アイコンと効果音が変わります"), 
+            w!("Win32 APIはw!でテキストを設定"), 
+            MB_ICONERROR);
+
+        ShellMessageBoxW(None, None, w!("MB_* で他のアイコンも設定可能"), w!("World"), MB_ICONWARNING);
+    }
+}
